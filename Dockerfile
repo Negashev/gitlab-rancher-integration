b1f0d2ead8000fcd108c820daf279eddad88174f
Aleksandr Negashev <Aleksandr_Negashev@epam.com>
2020-09-29 11:07:49 +0500

    event pods create

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM python:alpine
 CMD [ "python", "-u", "run.py" ]
 WORKDIR /src
+
 ADD requirements.txt requirements.txt
-RUN pip --no-cache install -r requirements.txt
+
+RUN apk add --no-cache py3-gevent && \
+    apk add --no-cache --virtual .build-deps build-base libffi-dev libmemcached-dev zlib-dev && \
+    pip --no-cache install -r requirements.txt && \
+    apk del .build-deps
+
 ADD *.py ./
--- run.py
+++ run.py
@@ -1,13 +1,16 @@
+from gevent import monkey
+monkey.patch_all()
+
 import yaml
 from flask import Flask, request, jsonify
 
 admission_controller = Flask(__name__)
 
-@admission_controller.route('/validate/namespaces', methods=['POST'])
+@admission_controller.route('/autostop/workloads', methods=['POST'])
 def deployment_webhook():
     request_info = request.get_json()
     print(yaml.dump(request_info, allow_unicode=True, default_flow_style=False))
-    return admission_response(True, "Namespace valid")
+    return admission_response(True, "Workload append to autostop queue")
 
 def admission_response(allowed, message):
     return jsonify({"response": {"allowed": allowed, "status": {"message": message}}})
