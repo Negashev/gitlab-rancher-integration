64c253e6e924f90a5cc1a6d91a565d24b6665021
Aleksandr Negashev <Aleksandr_Negashev@epam.com>
2020-10-02 18:05:00 +0500

    add simple MutatingWebhookConfiguration

--- routes.py
+++ routes.py
@@ -1,9 +1,14 @@
 import os
 import yaml
+import base64
+import jsonpatch
 from flask import current_app as app, request, jsonify
 from models import db, Workload
 
-IGNORE_NAMESPACE = os.getenv("IGNORE_NAMESPACE", "default,gitlab-managed-apps,gri,cattle-prometheus,cattle-system,ingress-nginx,kube-node-lease,kube-public,kube-system,security-scan,istio-system").split(",")
+IGNORE_NAMESPACE = os.getenv("IGNORE_NAMESPACE", "default,gitlab-managed-apps,gri,cattle-prometheus,cattle-system,ingress-nginx,kube-node-lease,kube-public,kube-system,security-scan,istio-system,knative-serving,knative-eventing,longhorn-system").split(",")
+ADD_IGNORE_NAMESPACE = os.getenv("ADD_IGNORE_NAMESPACE", "")
+
+IGNORE_NAMESPACE = IGNORE_NAMESPACE + ',' + ADD_IGNORE_NAMESPACE
 
 @app.route('/healthz', methods=['GET'])
 def healthz():
@@ -20,9 +25,7 @@ def workload_records():
         if i['kind'] in ['StatefulSet', 'ReplicaSet']:
             name = i['name']
             if i['kind'] =='ReplicaSet':
-                print(i)
                 name = i['name'].rpartition('-')[0]
-                print(name)
             workload = Workload(
                 workload=name,
                 workload_type=i['kind'],
@@ -42,3 +45,19 @@ def workload_records():
 
 def admission_response(allowed, message):
     return jsonify({"response": {"allowed": allowed, "status": {"message": message}}})
+
+
+@app.route('/mutate/workloads', methods=['POST'])
+def deployment_webhook_mutate():
+    request_info = request.get_json()
+    print(yaml.dump(request_info, allow_unicode=True, default_flow_style=False))
+    return admission_response_patch(True, "Adding allow label", json_patch = jsonpatch.JsonPatch([{"op": "add", "path": "/metadata/labels/allow", "value": "yes"}]))
+
+
+
+def admission_response_patch(allowed, message, json_patch):
+    base64_patch = base64.b64encode(json_patch.to_string().encode("utf-8")).decode("utf-8")
+    return jsonify({"response": {"allowed": allowed,
+                                 "status": {"message": message},
+                                 "patchType": "JSONPatch",
+                                 "patch": base64_patch}})
